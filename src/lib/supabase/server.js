32cbddd4023202c160852bec318766375abf7891
abhaysharma2719 <abhay@techcrumb.com>
2024-12-10 16:36:56 +0530

    added the console for api key

--- src/lib/supabase/server.js
+++ src/lib/supabase/server.js
@@ -2,6 +2,8 @@ import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
 export async function createClient() {
+    console.log(process.env.NEXT_PUBLIC_SUPABASE_URL,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY, "API KEYSSSSSSSSSSSSSSSSSSSSSSS")
     const cookieStore = await cookies()
 
     // Create a server's supabase client with newly configured cookie,

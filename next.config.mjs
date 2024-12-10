0a155f10e7b4adb0a3a975ec9d50fe06fcf2c619
abhaysharma2719 <abhay@techcrumb.com>
2024-12-10 17:41:50 +0530

    added pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
-// next.config.mjs
-export default {
-    experimental: {
-        serverActions: true, // Enable server actions
-    },
-};
+// next.config.js
+
+import runtimeCaching from 'next-pwa/cache.js';
+import withPWA from 'next-pwa';
+
+const nextConfig = withPWA({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
+    // next config
+});
+
+export default nextConfig
--- src/app/layout.js
+++ src/app/layout.js
@@ -20,6 +20,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >

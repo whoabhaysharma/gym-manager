f91924e2e152666cc068f26a26d5b2cd7d1021e3
abhaysharma2719 <abhay@techcrumb.com>
2024-12-10 13:49:06 +0530

    fixed the detail page

--- src/app/(app)/members/[memberId]/page.jsx
+++ src/app/(app)/members/[memberId]/page.jsx
@@ -1,41 +1,77 @@
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
-import { Card, CardHeader, CardBody } from "@/components/ui/card";
-import supabase from "@/lib/supabase/client";
-import MemberDetail from "./memberDetail";
+import moment from "moment";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+import { startCase, toLower } from "lodash";
+import { cookies } from 'next/headers';
 
-export default async function Page({ params }) {
-    const memberId = (await params).memberId;
+export default async function MemberDetail({ params }) {
+    const id = (await params).memberId;
+    const cookiesList = await cookies();
 
-    // Fetch the member's data and memberships from Supabase
-    // const getMemberData = async (id) => {
-    //     try {
-    //         // Fetch the member details
-    //         const { data: memberData, error: memberError } = await supabase
-    //             .from("members")
-    //             .select("*")
+    let memberData, membershipsData;
 
-    //         console.log(memberData, 'MEMBER DATA')
+    try {
+        const memberResponse = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/members/${id}`, {
+            headers: {
+                'Cookie': cookiesList.toString()
+            }
+        });
 
-    //         if (memberError) throw memberError;
+        memberData = await memberResponse.json();
 
-    //         // Fetch the memberships for the member
-    //         const { data: membershipsData, error: membershipsError } = await supabase
-    //             .from("memberships")
-    //             .select("*")
-    //             .eq("member_id", memberData.id);
+        const membershipsResponse = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/members/${memberData.member.id}/memberships`, {
+            headers: {
+                'Cookie': cookiesList.toString()
+            }
+        });
 
-    //         if (membershipsError) throw membershipsError;
+        membershipsData = await membershipsResponse.json();
 
-    //         return { memberData, membershipsData };
-    //     } catch (error) {
-    //         console.error("Error fetching data:", error);
-    //         return { memberData: null, membershipsData: [] };
-    //     }
-    // };
+    } catch (err) {
+        return <div>Error: {err.message}</div>;
+    }
 
-    // const { memberData, membershipsData } = await getMemberData(memberId);
+    const mom = moment(memberData.member.joining_date)
 
     return (
-        <MemberDetail id={memberId} />
+        <div>
+            <div className="mb-4 flex flex-row justify-end">
+                <Button>Add Membership +</Button>
+            </div>
+            <Card>
+                <CardHeader>
+                    <div className="flex flex-row justify-between">
+                        <div>
+                            <h2 className="text-3xl font-bold mb-2">{startCase(toLower(memberData.member.name))}</h2>
+                            <div>
+                                <Badge>{memberData.member.bill_number}</Badge>
+                            </div>
+                        </div>
+                        <p className="text-xs text-slate-600">
+                            Joined on {mom.calendar()}
+                        </p>
+                    </div>
+                </CardHeader>
+            </Card>
+
+            <Card className="mt-3">
+                <CardHeader>
+                    <h1 className="text-lg">Memberships</h1>
+                </CardHeader>
+                <CardContent>
+                    {(membershipsData?.membership || []).map((membership) => (
+                        <div key={membership.id}>
+                            <div className="w-full flex flex-row justify-between">
+                                <p>Start Date: {moment(membership.start_date).format("DD-MM-YYYY")}</p>
+                                <Badge>{`${membership.duration} Month`}</Badge>
+                            </div>
+                            <Separator className="mt-4 mb-4" />
+                        </div>
+                    ))}
+                </CardContent>
+            </Card>
+        </div>
     );
 }

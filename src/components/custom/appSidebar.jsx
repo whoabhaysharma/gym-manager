066e6c4cc9c41ac39c5208bf82f72208945d9505
abhaysharma2719 <abhay@techcrumb.com>
2024-12-10 16:04:13 +0530

    fixed the sidebar

--- src/components/custom/appSidebar.jsx
+++ src/components/custom/appSidebar.jsx
@@ -7,22 +7,24 @@ import {
     SidebarMenuButton,
     SidebarMenuItem,
 } from "@/components/ui/sidebar"
+import { Separator } from "../ui/separator"
+import { Users } from "lucide-react"
 
 export function AppSidebar() {
     return (
         <Sidebar>
             <SidebarHeader>
-                This is my header
+                <div>
+                    <h1 className={"text-2xl font-bold text-center"}>Fitbull Manager</h1>
+                    <Separator className="mt-2 mb-2" />
+                </div>
             </SidebarHeader>
             <SidebarContent>
                 <SidebarGroup>
-                    <SidebarMenuItem key={"title"}>
-                        <SidebarMenuButton asChild>
-                            <a href={"/members"}>
-                                <span>{"Members"}</span>
-                            </a>
-                        </SidebarMenuButton>
-                    </SidebarMenuItem>
+                    <a className="px-4 py-2 rounded-md transition-all flex gap-3 items-center bg-slate-100" href={"/members"}>
+                        <Users size={15} />
+                        <span>{"Members"}</span>
+                    </a>
                 </SidebarGroup>
                 <SidebarGroup />
             </SidebarContent>
